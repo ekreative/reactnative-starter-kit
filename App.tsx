0025a712fe6ff6bd354c97de463dd26995763f5a
DmytroTrubetskoi <dmytro.trubetskoi@ekreative.com>
2020-02-20 15:59:24 +0200

    kept changes to a minimum

--- App.tsx
+++ App.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-// import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ScreenA from './app/screens/ScreenA/ScreenA';
-import ScreenB from './app/screens/ScreenB/ScreenB';
-import ScreenC from './app/screens/ScreenC/ScreenC';
-import ScreenWithHooks from './app/screens/ScreenWithHooks/ScreenWithHooks';
 import { store, persistor } from './app/config/store';
-// import AppNavigator from './app/config/routes';
+import { AppNavigator } from './app/config/routes';
 import ErrorBoundary from './app/components/ErrorBoundary/ErrorBoundary';
-
-const Stack = createStackNavigator();
 
 const App = () => {
   return (
@@ -20,15 +12,7 @@ const App = () => {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <NavigationContainer>
-            <Stack.Navigator initialRouteName="ScreenA">
-              <Stack.Screen name="ScreenA" component={ScreenA} />
-              <Stack.Screen name="ScreenB" component={ScreenB} />
-              <Stack.Screen name="ScreenC" component={ScreenC} />
-              <Stack.Screen
-                name="ScreenWithHooks"
-                component={ScreenWithHooks}
-              />
-            </Stack.Navigator>
+            <AppNavigator />
           </NavigationContainer>
         </PersistGate>
       </Provider>
