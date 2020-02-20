a8f4289e4a465a4a04ff04b533b0fcc0816e1e71
DmytroTrubetskoi <dmytro.trubetskoi@ekreative.com>
2020-02-20 15:09:59 +0200

    navigation works. few screens added

--- App.tsx
+++ App.tsx
@@ -1,18 +1,50 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { store, persistor } from './app/config/store';
-import AppNavigator from './app/config/routes';
+// import AppNavigator from './app/config/routes';
 import ErrorBoundary from './app/components/ErrorBoundary/ErrorBoundary';
 
-function HomeScreen() {
+function HomeScreen({ navigation }: any) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here'
+          });
+        }}
+      />
+    </View>
+  );
+}
+function DetailsScreen({ route, navigation }: any) {
+  /* 2. Get the param */
+  const { itemId } = route.params;
+  const { otherParam } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100)
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -25,8 +57,9 @@ const App = () => {
       <ErrorBoundary>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
-            <Stack.Navigator>
-              <Stack.Screen name="Home" component={HomeScreen}/>
+            <Stack.Navigator initialRouteName="Home">
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Details" component={DetailsScreen} />
             </Stack.Navigator>
           </PersistGate>
         </Provider>
